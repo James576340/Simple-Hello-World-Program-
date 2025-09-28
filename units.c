13f91994f4b90f950ab1fe755e4fb353bd895678
IRUNGU JAMES NDIRANGU PA106/G/28731/25 <spenzajimmy82@gmail.com>
2025-09-28 13:23:55 +0300

    Create units.c

---
+++ units.c
@@ -0,0 +1,21 @@
+//Simple Hello World program
+/*
+Name: IRUNGU JAMES NDIRANGU 
+Reg No: PA106/G/28731/25
+Description: Program To Display all units for this Semester 
+*/
+
+#include <stdio.h> //Preprocessor directive printf(),scanf()
+
+//main function 
+int main() {
+             printf("Introduction to Programming and Problem solving\n");
+             printf("Computer systems and organisation\n");
+             printf("Communication skills\n");
+             printf("Fundamentals of System Analysis and Design\n");
+             printf("Mathematics for science\n");
+             printf("Discrete Mathematics\n");
+             printf("Fundamentals of ICT\n");
+             
+      return 0;
+        }
