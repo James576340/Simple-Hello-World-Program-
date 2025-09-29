852a71764d9f3c5e99e132a35db5f572e1fb90c1
IRUNGU JAMES NDIRANGU PA106/G/28731/25 <spenzajimmy82@gmail.com>
2025-09-29 13:34:00 +0300

    Add files via upload

---
+++ exam_eligibility.c
@@ -0,0 +1,30 @@
+//Hello World Program 
+/*Name: IRUNGU JAMES NDIRANGU 
+Reg No: PA106/G/28731/25
+Description: Program To check exam legibility of a student by attendance and average marks
+*/
+
+#include<stdio.h>
+//main function 
+int main(){
+          float attendance ;
+          float averagemarks;
+          
+          //Prompt User for Input
+     printf("Enter your attendance percentage:");
+     scanf("%f",& attendance);
+     printf("Enter your average marks:");
+     scanf("%f",& averagemarks);
+     // Conditions
+     if(attendance>=75 && averagemarks>=40){
+     printf("Eligible");
+     } else{
+           printf("Not Eligible");
+           }
+          
+      return 0;
+
+
+
+
+}
