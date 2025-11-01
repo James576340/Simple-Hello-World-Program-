f92c345ad5d5d77b3919bb10be558022e837a937
IRUNGU JAMES NDIRANGU PA106/G/28731/25 <spenzajimmy82@gmail.com>
2025-11-01 18:37:22 +0300

    Add files via upload

---
+++ Functions_calculateElectricBill.c
@@ -0,0 +1,30 @@
+/* 
+Name: IRUNGU JAMES NDIRANGU 
+Reg Number: PA106/G/28731/25
+Description: Functions 
+*/
+
+#include <stdio.h>
+
+float calculateElectricBill(int units) {
+    float bill = 0;
+
+    if (units <= 100) {
+        bill = units * 10;
+    } else if (units <= 200) {
+        bill = (100 * 10) + (units - 100) * 15;
+    } else {
+        bill = (100 * 10) + (100 * 15) + (units - 200) * 20;
+    }
+
+    return bill;
+}
+
+int main() {
+    int units;
+    printf("Enter the number of units consumed: ");
+    scanf("%d", &units);
+
+    printf("Total bill = KSh. %.2f\n", calculateElectricBill(units));
+    return 0;
+}
